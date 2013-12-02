104e4b5955a274b9419f8cdbdd11204b6b254276
Jian Zhen <zhenjl@gmail.com>
2013-12-01 19:24:01 -0800

    more test updates

--- bytebuffer/ringbuffer_test.go
+++ bytebuffer/ringbuffer_test.go
@@ -535,7 +535,7 @@ func Benchmark1ProducerAnd2Consumers(b *testing.B) {
 	}
 }
 
-func BenchmarkChannels(b *testing.B) {
+func BenchmarkChannels1Consumer(b *testing.B) {
 	dataSize := 256
 	data := make([]byte, dataSize)
 	for i := 0; i < dataSize; i++ {
@@ -553,6 +553,41 @@ func BenchmarkChannels(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		out := <-ch
+		if !bytes.Equal(out, data) {
+			b.Fatalf("bytes not the same")
+		}
+	}
+}
+
+func BenchmarkChannels2Consumers(b *testing.B) {
+	dataSize := 256
+	data := make([]byte, dataSize)
+	for i := 0; i < dataSize; i++ {
+		data[i] = byte(i % 256)
+	}
+
+	ch := make(chan []byte, 128)
+	ch2 := make(chan []byte, 128)
+	go func() {
+		for i := 0; i < b.N; i++ {
+			tmp := make([]byte, dataSize)
+			copy(tmp, data)
+			ch <- tmp
+			ch2 <- tmp
+		}
+	}()
+
+	go func() {
+		for i := 0; i < b.N; i++ {
+			out := <-ch
+			if !bytes.Equal(out, data) {
+				b.Fatalf("bytes not the same")
+			}
+		}		
+	}()
+	
+	for i := 0; i < b.N; i++ {
+		out := <-ch2
 		if !bytes.Equal(out, data) {
 			b.Fatalf("bytes not the same")
 		}
